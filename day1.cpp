c582b97bbad40bf6b10e6ca24a02fd924f90141c
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-06-29 12:38:04 +0530

    Add files via upload

---
+++ day1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+int main()
+{
+    int a=4,i;
+    double b=4.0,d;
+    string c="HackerRank",s;
+    cin>>i;
+    cin>>d;
+    getline (cin>>ws,s);
+    cout<<a+i<<endl;
+    cout<<fixed<<setprecision(1)<<b+d<<endl;
+    cout<<c+" "+s;
+    getch();
+    return 0;
+}
